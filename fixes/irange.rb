02a6757f42e775c7cae94704c68cdaea110e1a86
Michal Molhanec <Michal Molhanec>
2016-10-01 20:08:14 +0200

    Initial commit

---
+++ fixes/irange.rb
@@ -0,0 +1,29 @@
+class UnprocessedNode
+  # this one is different: we work on the level above irange which should be begin sexpr
+  def fix_irange_in_begin
+    if symbol? 'begin' and children.size == 1 and child.symbol? 'irange'
+      return IRangeNode.new child
+    end
+    self
+  end
+end
+
+
+
+class IRangeNode < Node
+
+  alias from first_child
+  alias to second_child
+
+  def initialize(irange_node)
+    super(irange_node.ruby_node)
+    irange_node.expect_len 2
+    assign_children irange_node.children
+  end
+
+  def to_s
+    'IRange'
+  end
+
+  def cls; self; end
+end
